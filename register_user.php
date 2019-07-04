c9f7a29bcc511a72edc3c932fe805fd76fb18790
hison731 <mjone352@gmail.com>
2019-07-04 09:02:04 +0800

    register-complete

--- register_user.php
+++ register_user.php
@@ -6,11 +6,17 @@ $username = $_POST['username'];
 $password = $_POST['password'];
 $refferal = $_POST['refferal'];
 
-$tsql = "insert into SystemUsers (Username, Password, Referral) values ('$username', '$password', '$refferal')";
+$tsql = "insert into SystemUsers (Username, Password, Referral) values ('$username', '$password', '$refferal'); SELECT SCOPE_IDENTITY() AS IDENTITY_COLUMN_NAME";
 
 $stmt = sqlsrv_query( $conn, $tsql);
-$obj = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC);
-print_r($stmt);
 
+sqlsrv_next_result($stmt);
+sqlsrv_fetch($stmt);
+$id = sqlsrv_get_field($stmt, 0);
+
+$_SESSION["username"] = $username;
+$_SESSION["id"] = $id;
+
+header("location:dashboard.php");
 
 ?>
